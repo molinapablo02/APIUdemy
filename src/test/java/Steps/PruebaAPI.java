af7f9cb5dec75252fe4f0a1c6155d60e7c81ea2e
molinapablo02 <molina.pablo02@gmail.com>
2022-04-27 15:26:03 -0300

    se agrega oauth2

--- src/test/java/Steps/PruebaAPI.java
+++ src/test/java/Steps/PruebaAPI.java
@@ -2,15 +2,18 @@ package Steps;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.Base64;
 
-
-
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class PruebaAPI {
 
     public void GetRequest(){
 
-        given().baseUri("https://api.github.com")
+        given()
+        .baseUri("https://api.github.com")
         .when()
         .get("/users")
         .getBody();
@@ -54,6 +57,82 @@ public class PruebaAPI {
         .assertThat().statusCode(200);
 
     }
+
+    /*
+        1- Obtener el codigo del servicio original para obtener el token
+        2- Obtener el token, intercambiando el codigo que obtuvimos 
+        3- Acceder al recurso protegido con nuestro token
+    */
+
+    public static String clientId = "";
+    public static String redirectUri = "";
+    public static String scope = "";
+    public static String userName = "";
+    public static String password = "";
+    public static String grantType = "";
+    public static String accessToken = "";
+    public static String encode(String str1, String str2){
+        return new String(Base64.getEncoder().encode((str1+ ":" + str2).getBytes()));
+    }
+
+    public static Response getCode(){
+        String authorization = encode(userName, password);
+
+        return
+                given()
+                .header("authorization","Basic" + authorization)
+                .contentType(ContentType.URLENC)
+                .formParam("response_type","code")
+                .queryParam("cliente_id", clientId)
+                .queryParam("redirect_uri", redirectUri)
+                .queryParam("scope", scope)
+                .post("/oauth/authorize")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+                
+    }
+
+    //extraemos el codigo de la respuesta 
+
+    public static String parseForOauthCode(Response response){
+        return response.jsonPath().getString("code");
+    }
+
+    public static Response getToken(String authCode){
+        String authorization = encode(userName, password);
+
+        return 
+                given()
+                .header("authorization","Basic" + authorization)
+                .contentType(ContentType.URLENC)
+                .formParam("response_type", authCode)
+                .queryParam("redirect_uri", redirectUri)
+                .queryParam("grant_type", grantType)
+                .post("/oauth/token")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+    }
+
+    public static String parseForToken(Response logiResponse){
+        return logiResponse.jsonPath().getString("access_token");
+    }
+
+    public static void getFinalService(){
+
+        given().auth().oauth2(accessToken)
+        .when()
+        .get("/service")
+        .then()
+        .statusCode(200);
+        
+        
+    }
+
 }
 
 
